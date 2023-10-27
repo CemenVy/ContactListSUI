95dc1ac86f0ec392fc8e6f074b054273cc7dd299
Семен Выдрин <pw5hv7ngcv@privaterelay.appleid.com>
2023-10-27 16:22:35 +0400

    Initial Commit

---
+++ ContactListSUI/ContactListSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSUIApp.swift
+//  ContactListSUI
+//
+//  Created by Семен Выдрин on 27.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
